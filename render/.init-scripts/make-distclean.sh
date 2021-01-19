7e87cf2f642b59f71b49d3fca9c5016203ba1a26
Peter Brightwell <peter.brightwell@bbc.co.uk>
2021-01-19 16:53:27 +0000

    lint and render dirs

--- render/.init-scripts/make-distclean.sh
+++ render/.init-scripts/make-distclean.sh
@@ -2,5 +2,5 @@
 
 set -o errexit
 
-rm -rf source-repo branches tags index.md index-contents.md
+rm -rf source-repo branches releases index.md index-contents.md
 rm -rf node_modules/ yarn.lock package-lock.json .scripts/ .layouts/ _layouts/ assets/ raml2html-nmos-theme/ _site/
