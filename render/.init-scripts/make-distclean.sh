e9fcd52f18f95a143733408c2d2c111b6512ca24
Peter Brightwell <peter.brightwell@bbc.co.uk>
2021-01-19 18:16:00 +0000

    Better distclean

--- render/.init-scripts/make-distclean.sh
+++ render/.init-scripts/make-distclean.sh
@@ -2,5 +2,8 @@
 
 set -o errexit
 
-rm -rf source-repo branches releases index.md index-contents.md
-rm -rf node_modules/ yarn.lock package-lock.json .scripts/ .layouts/ _layouts/ assets/ raml2html-nmos-theme/ _site/
+rm -f index.md index-contents.md
+rm -rf source-repo/ branches/ releases/
+rm -rf .scripts/ .layouts/ _layouts/ assets/ raml2html-nmos-theme/ _site/ .ssh/
+rm -rf node_modules/
+rm -f yarn.lock package.json package-lock.json Gemfile.lock
